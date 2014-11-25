2cbe3dbcf121bc10f1c1798925e1507831e32317
Satoshi Kay ISHII <ishiisk@gmail.com>
2014-11-25 08:59:48 -0800

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    url( r'^$', 'lists.views.home_page', name='home' )
+    url( r'^$', 'lists.views.home_page', name='home' ),
+    url( r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+        name = 'view_list'
+    ),
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 

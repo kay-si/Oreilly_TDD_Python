fa10703e4eaf70eb7bb5720c90fc10e5726c12bf
Satoshi Kay ISHII <ishiisk@gmail.com>
2014-11-26 08:35:33 -0800

    new URL + view for adding to existing lists. FT passes :-)

--- lists/tests.py
+++ lists/tests.py
@@ -73,6 +73,12 @@ class ListViewTest( TestCase ):
         self.assertNotContains( response, 'other item 1' )
         self.assertNotContains( response, 'other item 2' )
 
+    def test_passses_correct_list_to_template( self ):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get( '/lists/%d/' % ( correct_list.id, ) )
+        self.assertEqual( response.context['list'], correct_list )
+
 class NewListTest( TestCase ):
     def test_can_save_a_POST_request_to_an_existing_list( self ):
         other_list = List.objects.create()

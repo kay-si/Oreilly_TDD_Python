b47f5803c141391f8e39eaf4df79d7a3da30abb0
Satoshi Kay ISHII <ishiisk@gmail.com>
2014-11-29 18:32:00 -0800

    rename all item input ids and names. still broken

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,19 +8,6 @@ from lists.views import home_page
 from lists.forms import ItemForm
 
 class HomePageTest( TestCase ):
-#    maxDiff = None
-#    def test_root_url_resolves_to_home_page_view( self ):
-#        found = resolve( '/' )
-#        self.assertEqual( found.func, home_page )
-#
-#    def test_home_page_returns_correct_html( self  ):
-#        request = HttpRequest()
-#        response = home_page( request )
-#        expected_html = render_to_string( 'home.html' )
-#        self.assertTrue( response.content.decode(), expected_html )
-#        self.assertTrue( response.content.startswith( b'<html>' ) )
-#        self.assertIn( b'<title>To-Do lists</title>', response.content )
-#        self.assertTrue( response.content.strip().endswith( b'</html>' ) )
 
     def test_home_page_renders_home_template( self ):
         response = self.client.get( '/' )
@@ -29,12 +16,6 @@ class HomePageTest( TestCase ):
     def test_home_page_uses_item_form( self ):
         response = self.client.get( '/' )
         self.assertIsInstance( response.context['form'], ItemForm )
-
-#    def test_home_page_returns_correct_html( self ):
-#        request = HttpRequest()
-#        response = home_page( request )
-#        expected_html = render_to_string( 'home.html', {'form': ItemForm() } )
-#        self.assertMultiLineEqual( response.content.decode(), expected_html )
 
 class ListViewTest( TestCase ):
     def test_uses_list_template( self ):

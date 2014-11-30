24cd747af2a5fac83a955ba0cafeb7c438b26a2c
Satoshi Kay ISHII <ishiisk@gmail.com>
2014-11-29 20:13:58 -0800

    End of chapter 11

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 
 from lists.forms import EMPTY_LIST_ERROR, ItemForm
+from lists.models import Item, List
 
 class ItemFormTest( TestCase ):
 
-    def test_from_renders_item_text_input( self ):
+    def test_form_renders_item_text_input( self ):
         form = ItemForm()
         self. assertIn( 'placeholder="Enter a to-do item"', form.as_p() )
         self. assertIn( 'class="form-control input-lg"', form.as_p() )
@@ -16,3 +17,15 @@ class ItemFormTest( TestCase ):
             form.errors['text'],
             [ EMPTY_LIST_ERROR ],
         )
+
+    def test_form_save_handles_saving_to_a_list( self ):
+        form = ItemForm( data= {'text': 'do me' } )
+        new_item = form.save()
+
+    def test_form_save_handles_saving_to_a_list( self ):
+        list_ = List.objects.create()
+        form  = ItemForm( data={'text': 'do me'} )
+        new_item = form.save(for_list=list_)
+        self.assertEqual( new_item, Item.objects.first() )
+        self.assertEqual( new_item.text, 'do me' )
+        self.assertEqual( new_item.list, list_ )

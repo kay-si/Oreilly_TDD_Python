1114144d2851bff17619351554fff9da7abbbf8b
Satoshi Kay ISHII <ishiisk@gmail.com>
2014-11-30 22:42:59 -0800

    end of chapter 13

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,12 @@ from django.template.loader import render_to_string
 from django.utils.html import escape
 from lists.models import Item, List
 from lists.views import home_page
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import (
+        EMPTY_LIST_ERROR,
+        DUPLICATE_ITEM_ERROR,
+        ExistingListItemForm,
+        ItemForm,
+)
 from unittest import skip
 
 class HomePageTest( TestCase ):
@@ -73,7 +78,7 @@ class ListViewTest( TestCase ):
     def test_displays_item_form( self ):
         list_ = List.objects.create()
         response = self.client.get( '/lists/%d/' % ( list_.id, ) )
-        self.assertIsInstance( response.context['form'], ItemForm )
+        self.assertIsInstance( response.context['form'], ExistingListItemForm )
         self.assertContains( response, 'name="text"' )
 
     def post_invalid_input( self ):
@@ -94,7 +99,7 @@ class ListViewTest( TestCase ):
 
     def test_for_invalid_input_passes_list_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance( response.context['form'], ItemForm )
+        self.assertIsInstance( response.context['form'], ExistingListItemForm )
 
     def test_for_invalid_input_shows_error_on_page( self ):
         response = self.post_invalid_input()
@@ -108,7 +113,7 @@ class ListViewTest( TestCase ):
             '/lists/%d/' % ( list1.id, ),
             data={'text': 'textey' },
         )
-        expected_error = escape( "You've already got this in your list")
+        expected_error = escape( DUPLICATE_ITEM_ERROR )
         self.assertContains( response, expected_error )
         self.assertTemplateUsed( response, 'list.html' )
         self.assertEqual( Item.objects.all().count(), 1 )

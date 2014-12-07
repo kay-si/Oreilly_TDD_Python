e6df86e8bf53b7712667b8ca07e8ed8c599b4837
Satoshi Kay ISHII <ishiisk@gmail.com>
2014-12-06 23:34:49 -0800

    placeholder test_my_lists and move login checkers into base

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -11,7 +11,7 @@ class MyListTest( FunctionalTest ):
     def create_pre_authenticated_session( self, email ):
         user = User.objects.create( email= email )
         session = SessionStore()
-        session[ SESSION_KEY ] user.pk
+        session[ SESSION_KEY ]  = user.pk
         session[ BACKEND_SESSION_KEY ] = settings.AUTHENTICATION_BACKENDS[0]
         session.save()
 
@@ -23,3 +23,15 @@ class MyListTest( FunctionalTest ):
             value= session.session_key,
             path= '/',
         ))
+
+    def test_logged_in_user_lists_are_saved_as_my_lists( self ):
+        email= 'edith@example.com'
+
+        self.browser.get( self. server_url )
+        self.wait_to_be_logged_out( email )
+
+        # Edith is a logged-in user
+        self.create_pre_authenticated_session( email )
+
+        self.browser.get( self. server_url )
+        self.wait_to_be_logged_in( email )

933ff0f03ffd5e5e07da15b0e9fed436800a3a29
Satoshi Kay ISHII <ishiisk@gmail.com>
2014-12-07 17:51:28 -0800

    Remove some test comment

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -14,20 +14,6 @@ class MyListTest( FunctionalTest ):
             session_key = create_session_on_server( self.server_host, email)
         else:
             session_key = create_pre_authenticated_session( email )
-#        user = User.objects.create( email= email )
-#        session = SessionStore()
-#        session[ SESSION_KEY ]  = user.pk
-#        session[ BACKEND_SESSION_KEY ] = settings.AUTHENTICATION_BACKENDS[0]
-#        session.save()
-
-        ## to set a cookie we need to first visit the domain.
-
-#def create_pre_authenticated_session(email):
-#    user = User.objects.create(email=email)
-#    session = SessionStore()
-#    session[SESSION_KEY] = user.pk
-#    session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0] session.save()
-#    return session.session_key
         ## 404 pages load the quickest!
         self.browser.get( self.server_url + '/404_no_such_url/' )
         self.browser.add_cookie( dict(
@@ -37,13 +23,36 @@ class MyListTest( FunctionalTest ):
         ))
 
     def test_logged_in_user_lists_are_saved_as_my_lists( self ):
-        email= 'edith@example.com'
-
-        self.browser.get( self. server_url )
-        self.wait_to_be_logged_out( email )
-
         # Edith is a logged-in user
-        self.create_pre_authenticated_session( email )
+        self.create_pre_authenticated_session( 'edith@example.com' )
 
+        # She goes to the home page and starts a list
         self.browser.get( self. server_url )
-        self.wait_to_be_logged_in( email )
+        self.get_item_input_box(). send_keys( 'Reticulate splines\n' )
+        self.get_item_input_box(). send_keys( 'Immanetize eschaton\n' )
+        first_list_url = self.browser.current_url
+
+        # She notices a "My lists" link, for the first time.
+        self.browser.find_element_by_link_text( 'My lists' ).click()
+
+        # She sees that her list is in there, named according to its
+        # first list item
+        self.browser.find_element_by_link_text( 'Reticulate splines' ).click()
+        self.assertEqual( self.browser.current_url, first_list_url )
+
+        # She decides to start another list, just to see
+        self.browser.get( self. server_url )
+        self.get_item_input_box(). send_keys( 'Click cows\n' )
+        second_list_url = self.browser.current_url
+
+        # Under "my lists", her new list appears
+        self.get_item_input_box(). send_keys( 'My lists\n' )
+        self.get_item_input_box(). send_keys( 'Click cows\n' )
+        self.assertEqual( self.browser.current_url, second_list_url )
+
+        # She logs out. The "My lists" option disappears
+        self.brwoser.find_element_by_id( 'id_logout') .click ()
+        self.assertEqual(
+            self.browser.find_elements_by_link_text( 'My lists' ),
+            []
+        )
